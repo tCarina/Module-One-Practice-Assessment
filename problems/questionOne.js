37e880a37e20feab27b8a786df5b6f74ab81fc83
Ben Stone <benjamin.h.stone@gmail.com>
2020-09-21 10:33:19 -0400

    add q1

---
+++ problems/questionOne.js
@@ -0,0 +1,13 @@
+/**
+ *
+
+* Determines whether or not a number is odd.
+
+* @param {number} n - The input number.
+* @returns {boolean} - True if n is odd, false if n is even.
+
+*/
+
+function isOdd() {
+
+}
