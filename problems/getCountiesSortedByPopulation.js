744a85ba733f3032856f4a9a478959bb39162fa7
Ben Stone <benjamin.h.stone@gmail.com>
2020-09-21 12:50:58 -0400

    add removeEven strings and tests

--- problems/getCountiesSortedByPopulation.js
+++ problems/getCountiesSortedByPopulation.js
@@ -1,8 +1,3 @@
-// Question Five:
-// Write a function called getLocationsSortedBySize that takes in an array of objects that look like the array below,
-// and returns an array of the strings corresponding to the value of the location property
-// The output should be in the same order as the input
-
 /**
 
 * Returns an array of country names sorted in descending order by population
---
+++ problems/removeEvenStrings.js
@@ -0,0 +1,14 @@
+/**
+
+* Removes all strings with an even character count from an array of strings.
+
+* @param {string[]} arr - The input array of strings
+* @returns {string[]} - Returns the strings in arr that have an odd number of characters
+
+*/
+
+function removeEvenStrings() {
+
+}
+
+module.exports = removeEvenStrings
