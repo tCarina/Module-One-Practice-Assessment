9f9612975ef3dee1a76091e073721f196607caa5
Ben Stone <benjamin.h.stone@gmail.com>
2020-09-21 11:18:10 -0400

    add second smallest and tests

---
+++ problems/secondSmallest.js
@@ -0,0 +1,14 @@
+/**
+
+* Return the second smallest number in an array of numbers
+
+* @param {number[]} arr - The input array
+* @returns {number} - Returns the second smallest number.
+
+*/
+
+function secondSmallest() {
+
+}
+
+module.exports = secondSmallest
