cd78f2597388384c9bc5cb28485c4e0a23d2d126
Ben Stone <benjamin.h.stone@gmail.com>
2020-09-21 10:45:13 -0400

    add q1 test

---
+++ __tests__/isOdd.tests.js
@@ -0,0 +1,20 @@
+const isOdd = require("./../problems/isOdd.js")
+
+test('returns true on odd inputs', () => {
+  expect(isOdd(1)).toBe(true)
+  expect(isOdd(3)).toBe(true)
+  expect(isOdd(23425)).toBe(true)
+  expect(isOdd(1233467)).toBe(true)
+})
+
+test('returns false on even inputs', () => {
+  expect(isOdd(0)).toBe(false)
+  expect(isOdd(4)).toBe(false)
+  expect(isOdd(1424)).toBe(false)
+  expect(isOdd(4448)).toBe(false)ß
+})
+
+test('returns false on NaN inputs', () => {
+  expect(isOdd(NaN)).toBe(false)
+  expect(isOdd("Hello!")).toBe(false)
+})
--- problems/isOdd.js
+++ problems/isOdd.js
@@ -1,10 +1,9 @@
 /**
- *
 
 * Determines whether or not a number is odd.
 
 * @param {number} n - The input number.
-* @returns {boolean} - True if n is odd, false if n is even.
+* @returns {boolean} - True if n is odd, false if n is even, or not a number.
 
 */
 
