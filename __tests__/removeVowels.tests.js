53f7455d397e09516cfe3a9e5ec1431d6260fee8
Ben Stone <benjamin.h.stone@gmail.com>
2020-09-21 11:12:29 -0400

    add remove vowels and tests

---
+++ __tests__/removeVowels.tests.js
@@ -0,0 +1,15 @@
+const removeVowels = require("./../problems/removeVowels.js")
+
+test('removes vowels from an input string', () => {
+  expect(removeVowels("hello")).toBe("hll")
+  expect(removeVowels("What's up?")).toBe("Wht's p?")
+  expect(removeVowels("aeaeae")).toBe("")
+})
+
+test("does not remove y from an input string", () => {
+  expect(removeVowels("y doesn't count")).toBe("y dsn't cnt")
+})
+
+test("does remove capital letters from an input string", () => {
+  expect(removeVowels("CAPITAL LETTERS DO COUNT")).toBe("CPTL LTTRS D CNT")
+})
---
+++ problems/removeVowels.js
@@ -0,0 +1,14 @@
+/**
+
+* Removes all vowels from an input string.  Treat y as a consonant, not a vowel
+
+* @param {string} str - The input string
+* @returns {string} - Returns a new string without any vowels.
+
+*/
+
+function removeVowels() {
+
+}
+
+module.exports = removeVowels
