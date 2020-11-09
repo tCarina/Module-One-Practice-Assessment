ce2c380dbdbf02ffa6a7fe6efea9faa4cd250c79
Jimmy Byess <jimmy@skelios.com>
2020-11-09 12:57:36 -0500

    update package json, add tests

---
+++ problems/countNumbers.js
@@ -0,0 +1,17 @@
+
+/** 
+ * Count all the numbers in an array
+ * 
+ * @param {Number[]} arr - An array containing numbers
+ * @returns {Object} - an object where the key is the number, and the value is the count of that number
+ * 
+ * ex: countNumbers([5,5,1,6,7,2,3,5,6]) 
+ * returns { 5: 3, 1: 1, 6: 2, 2: 1, 3: 1, 7: 1 }
+ */
+
+function countNumbers() {
+ 
+}
+
+
+module.exports = countNumbers
