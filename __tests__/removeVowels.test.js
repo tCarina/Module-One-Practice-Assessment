f3d2b0008f5e4bb082914a1aa23660004fef564d
Jimmy Byess <jimmy@skelios.com>
2020-11-10 13:34:36 -0500

    vowels

--- __tests__/removeVowels.test.js
+++ __tests__/removeVowels.test.js
@@ -1,15 +1,18 @@
 const removeVowels = require("../problems/removeVowels.js")
 
-test('removes vowels from an input string', () => {
-  expect(removeVowels("hello")).toBe("hll")
-  expect(removeVowels("What's up?")).toBe("Wht's p?")
-  expect(removeVowels("aeaeae")).toBe("")
-})
+describe("removeVowels", () => {
+  test('removes vowels from an input string', () => {
+    expect(removeVowels("hello")).toBe("hll")
+    expect(removeVowels("What's up?")).toBe("Wht's p?")
+    expect(removeVowels("aeaeae")).toBe("")
+  })
 
-test("does not remove y from an input string", () => {
-  expect(removeVowels("y doesn't count")).toBe("y dsn't cnt")
-})
+  test("does not remove y from an input string", () => {
+    expect(removeVowels("y doesn't count")).toBe("y dsn't cnt")
+  })
 
-test("does remove capital letters from an input string", () => {
-  expect(removeVowels("CAPITAL LETTERS DO COUNT")).toBe("CPTL LTTRS D CNT")
+  test("does remove capital letters from an input string", () => {
+    expect(removeVowels("CAPITAL LETTERS DO COUNT")).toBe("CPTL LTTRS D CNT")
+  })
+
 })
--- problems/removeVowels.js
+++ problems/removeVowels.js
@@ -1,10 +1,9 @@
 /**
 
 * Removes all vowels from an input string.  Treat y as a consonant, not a vowel
-
+*
 * @param {string} str - The input string
 * @returns {string} - Returns a new string without any vowels.
-
 */
 
 function removeVowels() {
