3bc0ee329ae2f88e894079fe0f73134ce2acdc5a
Jimmy Byess <jimmy@skelios.com>
2020-11-10 12:51:34 -0500

    countries tests

--- problems/countNumbers.js
+++ problems/countNumbers.js
@@ -2,8 +2,8 @@
 /** 
  * Count all the numbers in an array
  * 
- * @param {Number[]} arr - An array containing numbers
- * @returns {Object} - an object where the key is the number, and the value is the count of that number
+ * @param {number[]} arr - An array containing numbers
+ * @returns {object} - an object where the key is the number, and the value is the count of that number
  * 
  * ex: countNumbers([5,5,1,6,7,2,3,5,6]) 
  * returns { 5: 3, 1: 1, 6: 2, 2: 1, 3: 1, 7: 1 }
--- problems/getCountriesSortedByPopulation.js
+++ problems/getCountriesSortedByPopulation.js
@@ -1,10 +1,10 @@
 /**
 
 * Returns an array of country names sorted in descending order by population
-
-* @param {object[]} arr - The input array.  Objects will be in the form: { country: "foo", population: 10 }
+* 
+* @param {object[]} arr - The input array. Objects will be in the form: { country: "foo", population: 10 }
 * @returns {string[]} - Returns an array of country names, sorted by their population in descending order
-
+* 
 */
 
 function getCountriesSortedByPopulation() {
